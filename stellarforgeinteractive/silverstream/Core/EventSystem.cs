c9b1de8219a4611cd789591710c231647c86e738
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-09-15 14:33:30 -0300

    Created EventSystem.cs

---
+++ stellarforgeinteractive/silverstream/Core/EventSystem.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+namespace ca.stellarforgeinteractive.silverstream.Core
+{
+    /// <summary>
+    /// Implements observer pattern.
+    /// </summary>
+    public static class EventSystem
+    {
+        /// <summary>
+        /// Called by GM during initialization.
+        /// </summary>
+        public static Action Init = _Init;
+
+        public static Action GameplayStart = DoNothing;
+
+        public static Action GameplayEnd = DoNothing;
+
+        public static Action PlayerDied = GameplayEnd;
+        
+        public static Action PlayerWon = GameplayEnd;
+
+        private static void DoNothing()
+        {
+            
+        }
+        
+        private static void _Init()
+        {
+            Debug.Log(new NotImplementedException());
+        }
+    }
+}
