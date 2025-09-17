1aadfe188e2a731ee97d390222518e8f22da8a08
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-09-17 16:53:17 -0300

    Added Stamina system.

---
+++ stellarforgeinteractive/silverstream/Player/DrainType.cs
@@ -0,0 +1,8 @@
+namespace ca.stellarforgeinteractive.silverstream.Player
+{
+    internal enum DrainType
+    {
+        Walk,
+        Jump
+    }
+}
---
+++ stellarforgeinteractive/silverstream/Player/PlayerStatController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using ca.stellarforgeinteractive.silverstream.Core;
+using UnityEngine;
+
+namespace ca.stellarforgeinteractive.silverstream.Player
+{
+    /// <summary>
+    /// Responsible for tracking upgrades, and values related to them.
+    /// </summary>
+    [Serializable]
+    public class PlayerStatController
+    {
+        public Action OutofStamina = EventSystem.DoNothing;
+        
+        [Header("Stamina")]
+        [SerializeField] int baseStamina=12000;
+        [Tooltip("How many percent stamina increases per level, divided by 100")]
+        [SerializeField] float staminaUpgradeValue=0.5f;
+        [SerializeField, Tooltip("Should we passively drain stamina?")] bool usePassiveDrain=true;
+        [SerializeField] int passiveDrain=1;
+        [SerializeField] int walkDrain=4;
+        [SerializeField] int jumpDrain=180;
+        
+        /// <summary>
+        /// The max stamina the player can have.
+        /// </summary>
+        public int MaxStamina { get => (int)(baseStamina * (1 + staminaUpgradeValue) * staminaLevel); }
+        public int CurrentStamina { get; private set; }
+        
+        // Upgrade levels
+        public int staminaLevel;
+
+        void UpdateStamina(DrainType[] actions)
+        {
+            foreach (DrainType a in actions)
+            {
+                try
+                {
+                    switch (a)
+                    {
+                        case DrainType.Walk: CurrentStamina -= walkDrain; break;
+                        case DrainType.Jump: CurrentStamina -= jumpDrain; break;
+                        default: throw new InvalidDataException("Unknown DrainType");
+                    }
+                }
+                catch (Exception e){ Debug.LogException(e);}
+            }
+
+            if (usePassiveDrain)
+            {
+                CurrentStamina -= passiveDrain;
+            }
+        }
+    }
+}
