feeadf44586e7805d29d178f74aefec9f974e468
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-09-24 10:37:32 -0300

    Updated stamina value

--- stellarforgeinteractive/silverstream/Player/PlayerStatController.cs
+++ stellarforgeinteractive/silverstream/Player/PlayerStatController.cs
@@ -15,7 +15,7 @@ namespace ca.stellarforgeinteractive.silverstream.Player
         public Action OutofStamina = EventSystem.DoNothing;
         
         [Header("Stamina")]
-        [SerializeField] int baseStamina=12000;
+        [SerializeField] int baseStamina=6000;
         [Tooltip("How many percent stamina increases per level, divided by 100")]
         [SerializeField] float staminaUpgradeValue=0.5f;
         [SerializeField, Tooltip("Should we passively drain stamina?")] bool usePassiveDrain=true;
