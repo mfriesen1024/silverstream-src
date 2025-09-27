9bf60248c0be1459c7fb77339987ebe2b24b61e6
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-09-27 15:06:15 -0300

    Create treat functionality.

---
+++ stellarforgeinteractive/silverstream/World/Treat.cs
@@ -0,0 +1,40 @@
+using System;
+using ca.stellarforgeinteractive.silverstream.Core;
+using ca.stellarforgeinteractive.silverstream.Player;
+using UnityEngine;
+
+namespace ca.stellarforgeinteractive.silverstream.World
+{
+    [RequireComponent(typeof(EventHelper))]
+    public class Treat:MonoBehaviour
+    {
+        [SerializeField] EventHelper eventHelper;
+
+        private void Start()
+        {
+            eventHelper ??= GetComponent<EventHelper>();
+            if (eventHelper != null)
+            {
+                throw new NullReferenceException("EventHelper is required!");
+            }
+            
+            EventSystem.GameplayStart+= GameplayStart;
+            eventHelper.TriggerEnter2D += TriggerEnter2D;
+        }
+
+        private void GameplayStart()
+        {
+            gameObject.SetActive(true);
+        }
+
+        private void TriggerEnter2D(Collider2D obj)
+        {
+            if (obj.TryGetComponent(out PlayerController ignored))
+            {
+                Debug.LogException(new NotImplementedException("Treats not implemented."));
+                
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
