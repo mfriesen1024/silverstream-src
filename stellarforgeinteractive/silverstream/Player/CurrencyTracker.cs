9c25972a98029e27e5eb77f8a96d21351274a164
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-09-29 18:48:53 -0300

    Wrote a method to purchase upgrades

--- stellarforgeinteractive/silverstream/Player/CurrencyTracker.cs
+++ stellarforgeinteractive/silverstream/Player/CurrencyTracker.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using ca.stellarforgeinteractive.silverstream.Core;
 using UnityEngine;
 
@@ -8,10 +9,10 @@ namespace ca.stellarforgeinteractive.silverstream.Player
         public const double TreatMultiplier = 5;
         public const double DistanceMultiplier = 0.5;
         public const double StaminaMultiplier = 0.0025;
-        
+
         public static CurrencyTracker Instance { get; private set; }
 
-        public int Currency {get; private set;}
+        public int Currency { get; private set; }
 
         static CurrencyTracker()
         {
@@ -30,15 +31,15 @@ namespace ca.stellarforgeinteractive.silverstream.Player
             {
                 PlayerStatController psc = PlayerStatController.Instance;
                 var staminaUsed = psc.MaxStamina - psc.CurrentStamina;
-                Currency += (int)(TreatsThisRun*TreatMultiplier);
-                Currency += (int)(PlayerController.Distance*DistanceMultiplier);
+                Currency += (int)(TreatsThisRun * TreatMultiplier);
+                Currency += (int)(PlayerController.Distance * DistanceMultiplier);
                 Currency += (int)(staminaUsed * StaminaMultiplier);
                 Debug.Log($"Currency tracking:\n" +
                           $"Treats: {TreatsThisRun} (x{TreatMultiplier})\n" +
                           $"Distance: {PlayerController.Distance} (x{DistanceMultiplier})\n" +
                           $"Stamina Used: {staminaUsed} (x{StaminaMultiplier})\n" +
                           $"New Value: {Currency}"
-                          );
+                );
             }
 
             void GameplayStart()
@@ -53,5 +54,35 @@ namespace ca.stellarforgeinteractive.silverstream.Player
         }
 
         public int TreatsThisRun;
+        public int[][] prices { get; private set; } = new[]
+        {
+            new[] { 50, 200, 800 },
+            new[] { int.MaxValue },
+            new[] { int.MaxValue }
+        };
+
+        public void TryPurchaseUprade(int index)
+        {
+            PlayerStatController psc = PlayerStatController.Instance;
+            // If we want the 0th (stamina) upgrade, check the price list for levels.
+            // We won't have any other multi-tiered upgrades, so this reduces complexity.
+            int level = index == 0 ? psc.StaminaLevel : 0;
+
+            int price = prices[index][level];
+
+            // Dont continue if the price is above money.
+            if (price > Currency)
+            {
+                return;
+            }
+
+            Currency -= price;
+            switch (index)
+            {
+                case 0: psc.StaminaLevel++; break;
+                case 1: psc.DashUnlocked = true; break;
+                case 2: psc.WallJumpUnlocked = true; break;
+            }
+        }
     }
 }
