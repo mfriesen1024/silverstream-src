05105e8058ca63a287c96eb4eb498b348154b8dd
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-09-29 15:04:52 -0300

    Track currency at end of run.

--- stellarforgeinteractive/silverstream/Player/CurrencyTracker.cs
+++ stellarforgeinteractive/silverstream/Player/CurrencyTracker.cs
@@ -5,7 +5,13 @@ namespace ca.stellarforgeinteractive.silverstream.Player
 {
     public class CurrencyTracker
     {
+        public const double TreatMultiplier = 5;
+        public const double DistanceMultiplier = 0.05;
+        public const double StaminaMultiplier = 0.025;
+        
         public static CurrencyTracker Instance { get; private set; }
+
+        public int Currency {get; private set;}
 
         static CurrencyTracker()
         {
@@ -16,7 +22,17 @@ namespace ca.stellarforgeinteractive.silverstream.Player
         {
             EventSystem.TreatCollected += TreatCollected;
             EventSystem.GameplayStart += GameplayStart;
+            EventSystem.PlayerDied += PlayerDied;
             // If/when we have a reset save we'll deal with it here.
+
+            // Update currency when player dies.
+            void PlayerDied()
+            {
+                PlayerStatController psc = PlayerStatController.Instance;
+                Currency += (int)(TreatsThisRun*TreatMultiplier);
+                Currency += (int)(PlayerController.Distance*DistanceMultiplier);
+                Currency += (int)((psc.MaxStamina - psc.CurrentStamina) * StaminaMultiplier);
+            }
 
             void GameplayStart()
             {
