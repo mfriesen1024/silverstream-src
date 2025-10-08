a7ceefd90d00cb022055de6d486bc84b1bfe0b68
mfriesen1024 <144160470+mfriesen1024@users.noreply.github.com>
2025-10-08 13:09:37 -0300

    Separated animation helper from main class.

--- stellarforgeinteractive/silverstream/Player/PlayerController.cs
+++ stellarforgeinteractive/silverstream/Player/PlayerController.cs
@@ -324,40 +324,5 @@ namespace ca.stellarforgeinteractive.silverstream.Player
             return linearVelocity;
         }
 
-        class AnimHelper
-        {
-            PlayerController pc;
-            Animator animator;
-            EventHelper selfNode;
-
-            public AnimHelper(EventHelper selfNode, PlayerController pc)
-            {
-                this.selfNode = selfNode;
-                this.pc = pc;
-                animator = pc.animator;
-                
-                this.selfNode.Process += Process;
-            }
-
-            void Process(float delta)
-            {
-                if (pc.jumpTicksLeft == pc.jumpTicks -1)
-                {
-                    animator.SetTrigger("jump");
-                }
-                animator.SetBool("dash", pc.dashTicksLeft > 0);
-
-                var boolMove = pc.inputHelper.BooleanMove;
-                if (boolMove.x > 0)
-                {
-                    animator.SetBool("right",true);
-                }
-                if (boolMove.x < 0)
-                {
-                    animator.SetBool("right",false);
-                }
-            }
-
-        }
     }
 }
